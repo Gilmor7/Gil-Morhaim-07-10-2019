22b36164327a00bd4c5051a672c6cd08dba5b94b
Gil Morhaim <gilov1997@gmail.com>
2019-10-07 18:15:08 +0300

    initialized redux

---
+++ src/redux/favorites/favorites.reducer.js
@@ -0,0 +1,10 @@
+const initialState = null;
+
+const favoritesReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default favoritesReducer;
