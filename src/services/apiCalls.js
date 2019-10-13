9c13e7882a532227ec5e818101c87bb0383fdd78
Gil Morhaim <gilov1997@gmail.com>
2019-10-13 23:53:47 +0300

    fixed mix content problem by changing the protocol to https on the accuweather api endpoint

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 import { apiKey } from '../keys';
 
 export const getCitiesOptions = str => {
-    return axios.get(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${str}`);
+    return axios.get(`https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${str}`);
 }
 
 export const getCurrentWeather = cityKey => {
-    return axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${apiKey}`);
+    return axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${apiKey}`);
 }
 
 export const getForecast = cityKey => {
-    return axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${apiKey}&metric=true`);
+    return axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${apiKey}&metric=true`);
 }
